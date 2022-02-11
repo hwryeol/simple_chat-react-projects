96dda64193764f8262802ea4b7baecc09b3342e9
hwryeol <hwryeol@gmail.com>
2022-02-11 11:34:21 +0900

    chore(App.js): css작업

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const app = require('express')();
+const path = require('path');
+const express = require('express');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http,{
     cors: {
@@ -12,6 +14,12 @@ const port = 3001;
 http.listen(port, ()=> {
   console.log("listening on *: " + port);
 })
+
+// app.use( express.static(path.join(__dirname,'public')));
+
+// app.get('/',(req,res)=>{
+//   res.sendFile(path.join(__dirname,'src/public/index.html'));
+// })
 
 io.on('connection', function (socket) {
   console.log(socket.id, 'Connected');
--- src/App.js
+++ src/App.js
@@ -15,32 +15,35 @@ function App() {
 
   useEffect(()=>{
     socket.on('msg', (data)=>{
-    const newData = [data,false]
-    setChatData(prev => [...prev, newData])
-    })
+      const newData = [data,false]
+      setChatData(prev => [newData,...prev])
+      })
   },[])
   
   return (
     <div className="App">
-    <input ref={inputref} type="text" onChange={(data)=>{
-     setEmitText(data.target.value);
-    }}/>
-    <button onClick={()=>{
-      if(emitText){
-        socket.emit('msg',emitText);
-        setEmitText("");
-        inputref.current.value = "";
-        const newChatData = [emitText,true]
-        setChatData(prev=>[...prev,newChatData]);
-      };
-      
-      }}>클릭</button>
     <ul>
       {chatData?.map((data) => {
-        console.log(`data:${data}`);
         return <li key={`list_key${data.id}`} className={data[1]?"abc":"bca"}>{data[0]}</li>
       })}
     </ul>
+    <div className="inputSection">
+      <form>
+      <input ref={inputref} type="text" onChange={(data)=>{
+      setEmitText(data.target.value);
+      }}/>
+      <input type="submit" value="클릭" onClick={(event)=>{
+        event.preventDefault();
+        if(emitText){
+          socket.emit('msg',emitText);
+          setEmitText("");
+          inputref.current.value = "";
+          const newChatData = [emitText,true]
+          setChatData(prev=>[newChatData,...prev]);
+        };
+        }}/>
+      </form>
+    </div>
     </div>
   );
 }
