9bc4551c5fec2b4a035b554639ea13e572d8d0c6
hwryeol <hwryeol@gmail.com>
2022-02-15 22:59:55 +0900

    fect(server): room 분리 기능 및 여러기능 추가
    1. username과 메시지를 분리
    2. 방안에 존재하는 user의 이름을 가져오는 함수 추가

--- server.js
+++ server.js
@@ -21,14 +21,50 @@ http.listen(port, ()=> {
 //   console.log(path.join(__dirname,'build/static'))
 //   res.sendFile(path.join(__dirname,'build\\index.html'));
 // })
+function getPublicRooms(){
+  let {rooms,sids} = io.sockets.adapter;
+  let publicRooms = [];
 
-io.on('connection', function (socket) {
-  console.log(socket.id, 'Connected');
-  socket.join("public");
-    io.to("public").emit('msg', `${socket.id} 연결 되었습니다.`);
+  rooms.forEach((_,key) => {
+    if(sids.get(key) === undefined){
+      publicRooms.push(key);
+    }
+  })
+  
+  return publicRooms;
+}
 
-  socket.on('msg', function(data){
-    console.log(data)
-    socket.to("public").emit('msg',`${socket.id}:${data}`);
+function getSocketFromID(socketID,namespace="/"){
+  return io.of(namespace).sockets.get(socketID);
+}
+
+function getUserList(roomName){
+  let userList = [];
+  io.sockets.adapter.rooms.get(roomName).forEach((value)=>{
+    userList.push(getSocketFromID(value).username);
+  });
+  return userList;
+}
+
+io.on('connection', (socket) => {
+  socket.on('username', (data=`user${socket.id.slice(0,5)}`)=>{
+    socket["username"] = data;
+  })
+  socket.emit('public_room',getPublicRooms());
+  
+  socket.on('enter_room', (roomName)=>{
+    let room_data = {
+      name:roomName,
+      // userList:getUserList(roomName),
+    }
+    socket.emit('room_data', room_data);
+    socket.join(roomName);
+
+    io.to("public").emit('notice', `${socket.username}님이 연결 되었습니다.`);
+
+    socket.on('msg', (data) => {
+      socket.to(roomName).emit('msg', socket.username, data);
+    });
+
   })
 })
--- src/App.js
+++ src/App.js
@@ -2,23 +2,42 @@ import {io} from "socket.io-client"
 import React from "react"
 import './App.css';
 import {useState,useEffect} from "react"
-
+import Modal from './modal.js'
 import icon from "./img/arrow.png"
 
 const socket = io('http://localhost:3001/')
 
 const listref = React.createRef();
 const inputref = React.createRef();
+const usernameref = React.createRef();
+const roomName = React.createRef();
+
+let username = "";
 
 
 function App() {
   const [emitText,setEmitText] = useState("");
   const [chatData,setChatData] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+  const closeModal = () => {
+    username = usernameref.current.value;
+    socket.emit('username',username);
+    setModalOpen(false);
+  };
+
 
   useEffect(()=>{
-    socket.on('msg', (data)=>{
-      console.log(data)
-      const newData = [data,false]
+    openModal();
+    socket.on('msg', (username,message)=>{
+      const newData = [username,message,"msg"]
+      setChatData(prev => [newData,...prev])
+      })
+    socket.on('notice', (data)=>{
+      const newData = [data,"notice"]
       setChatData(prev => [newData,...prev])
       })
   },[])
@@ -26,7 +45,7 @@ function App() {
   const emitMsg = () => {
       if(emitText){
         socket.emit('msg',emitText);
-        const newChatData = [emitText,true]
+        const newChatData = [username,emitText,"myChat"]
         setChatData(prev=>[newChatData,...prev]);
       };
       setEmitText("");
@@ -40,16 +59,24 @@ function App() {
       emitMsg();
     }
   }
+  const enter= (e)=>{
+    if(e.key == 'Enter' && !e.shiftKey){
+      e.preventDefault();
+      socket.emit('enter_room', roomName.current.value )
+    }
+  }
   
   return (
     <div className="App">
     <ul>
       {chatData?.map((data) => {
-        return <li key={`list_key${data.id}`} className={data[1]?"abc":"bca"}>{data[0]}</li>
+        console.log(data);
+        return <li key={`list_key${data.id}`} className={data[2]}>{`${data[0]},${data[1]}`}</li>
       })}
     </ul>
     <div className="inputSection">
       <form>
+      <input type="text" ref={roomName} onKeyPress={enter}></input>
       <textarea className="text-input" ref={inputref} onChange={(data)=>{
       setEmitText(data.target.value);
       }} onKeyPress={onKeyPress}/>
@@ -58,6 +85,9 @@ function App() {
         emitMsg();
         }}></input>
       </form>
+      <Modal open={modalOpen} close={closeModal} header="modal heading">
+        <input type="text" ref={usernameref}></input>
+      </Modal>
     </div>
     </div>
   );
